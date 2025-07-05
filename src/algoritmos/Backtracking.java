cfa9ac73aa5a7c4c3e3accbd0c23709329f897f3
Florencia <florencianzl73@gmail.com>
2025-07-05 16:50:37 -0300

    Correcciones realizadas

--- src/algoritmos/Backtracking.java
+++ src/algoritmos/Backtracking.java
@@ -7,7 +7,8 @@ import java.util.ArrayList;
 
 /*
 * Las máquinas ya vienen ordenadas desde el reader (se implementó Comparable en la clase Máquina).
-* Para la resolución con el algoritmo Backtracking, se lleva una solución parcial de secuencia y cantidad de piezas.
+* Para la resolución con el algoritmo Backtracking, se lleva una solución parcial de secuencia, cantidad de piezas y un índice para evitar reevaluar combinaciones ya consideradas,
+* para evitar generar estados innecesariamente.
 * Por cada máquina se verifica si la cantidad de piezas que produce más las acumuladas hasta el momento no supera el total de piezas a fabricar. Si es así,
 * si la secuencia final está vacía o la cantidad de máquinas de la secuencia parcial con la nueva máquina agregada es menor a la secuencia ya encontrada,
 * se agrega y continúa explorando recursivamente.
@@ -32,9 +33,8 @@ public class Backtracking {
         this.estados = 0;
     }
 
-
     public void backtracking(){
-        if (backtracking(new ArrayList<>(), 0)){
+        if (backtracking(new ArrayList<>(), 0,0)){
             System.out.println("El total de piezas producidas es de: " + piezasTotales);
             System.out.println("La secuencia de máquinas obtenidas es: " + secuencia);
             System.out.println("La cantidad de puestas en funcionamiento de las máquinas es de: " + secuencia.size());
@@ -44,19 +44,20 @@ public class Backtracking {
         }
     }
 
-    private boolean backtracking(ArrayList<String> secuenciaParcial, int piezasActuales){
+    private boolean backtracking(ArrayList<String> secuenciaParcial, int piezasActuales, int indiceInicio) {
         estados++;
-        if (piezasActuales == piezasTotales){
-            if (secuenciaParcial.size() < secuencia.size() || secuencia.isEmpty()){
+        if (piezasActuales == piezasTotales) {
+            if (secuenciaParcial.size() < secuencia.size() || secuencia.isEmpty()) {
                 secuencia.clear();
                 secuencia.addAll(secuenciaParcial);
             }
         } else {
-            for (Maquina m : maquinas){
-                if (piezasActuales + m.getCantPiezas() <= piezasTotales){
+            for (int i = indiceInicio; i < maquinas.size(); i++) {
+                Maquina m = maquinas.get(i);
+                if (piezasActuales + m.getCantPiezas() <= piezasTotales) {
                     if (secuenciaParcial.size() + 1 < secuencia.size() || secuencia.isEmpty()) {
                         secuenciaParcial.add(m.getIdMaquina());
-                        backtracking(secuenciaParcial, piezasActuales + m.getCantPiezas());
+                        backtracking(secuenciaParcial, piezasActuales + m.getCantPiezas(), i);
                         secuenciaParcial.remove(secuenciaParcial.size() - 1);
                     }
                 }
--- src/algoritmos/Greedy.java
+++ src/algoritmos/Greedy.java
@@ -8,18 +8,16 @@ import java.util.ArrayList;
 /*
 * Para la resolución con el algoritmo Greedy, se planteó una estrategia de selección de máquinas ordenadas de mayor a menor,
 * según la cantidad de piezas que puede producir cada máquina. Las máquinas ya vienen ordenadas desde el reader (se implementó Comparable en la clase Máquina).
-* En cada iteración se recorre la lista de máquinas y se selecciona la primera que su producción, sumada a las piezas acumuladas, no supere el total de piezas a fabricar.
-* En el caso que no se encuentre ninguna máquina que cumpla, se retorna null y se corta la busqueda indicando que no se encontró solución.
-* Si las piezas actuales son iguales a las piezas totales, se encontró una solución y se imprimen los resultados.
+* En cada iteración se toma la primera máquina de la lista y se intenta añadir a la solución tantas veces como sea posible.
+* Cuando ya no es posible añadirla, es decir, supera el total de piezas a fabricar, se elimina de la lista, ya que no es más una candidata válida.
+* Si se encuentra solución o la lista de máquinas está vacía, se rompe el bucle y se imprime el resultado obtenido, ya sea satisfactorio o sin solución.
 * También se lleva un contador de candidatos, que aumenta cada vez que una máquina es considerada para ser agregada a la solución.
 * */
-
 
 public class Greedy {
     private int piezasTotales;
     private ArrayList<Maquina> maquinas;
     private int candidatos;
-
 
     public Greedy(){
         CSVReader reader = new CSVReader("datasets/Maquinas.csv");
@@ -31,14 +29,14 @@ public class Greedy {
     public void greedy(){
         ArrayList<String> secuencia = new ArrayList<>();
         int piezasActuales = 0;
-        while (piezasActuales < piezasTotales){
-            Maquina maquina = getMaquina(piezasActuales);
-            if (maquina == null){
-                break;
-            }
-            secuencia.add(maquina.getIdMaquina());
-            piezasActuales += maquina.getCantPiezas();
-
+        while (piezasActuales < piezasTotales && !maquinas.isEmpty()){
+                Maquina maquina = maquinas.get(0);
+                candidatos++;
+                while (piezasActuales + maquina.getCantPiezas() <= piezasTotales) {
+                    secuencia.add(maquina.getIdMaquina());
+                    piezasActuales += maquina.getCantPiezas();
+                }
+                maquinas.remove(0);
         }
         if (piezasActuales == piezasTotales){
             System.out.println("El total de piezas producidas es de: " + piezasTotales);
@@ -48,15 +46,5 @@ public class Greedy {
         } else {
             System.out.println("No se encontró solución");
         }
-    }
-
-    private Maquina getMaquina(int piezasActuales){
-        for (Maquina m : maquinas) {
-            candidatos++;
-            if (piezasActuales + m.getCantPiezas() <= piezasTotales) {
-                return m;
-            }
-        }
-       return null;
     }
 }
